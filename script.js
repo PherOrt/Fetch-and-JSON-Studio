2b034bd3c3b87c67476ab14934a7a0faa4f44834
COrtega <christopheraortega@gmail.com>
2022-03-10 21:31:53 -0600

    update

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 // TODO: add code here
+let list = [];
+window.addEventListener("load", async function(){
+    
+    await fetch("https://handlers.education.launchcode.org/static/astronauts.json")
+    .then(function(response){ 
+        response.json().then(function(json){
+            console.log(json[0]);
+           astronauts =  document.getElementById("container");
+           
+        //   let highest
+        //   let sortedList
+        // for(let i = 0; i<json.length; i++){
+        //      highest = json[0];
+        //     if(highest.hoursInSpace < json[i].hoursInSpace){
+        //         highest =json[i];
+                
+        //     }
+        //     sortedList.push(highest);
+        //     json.splice()
+        // }
+
+        for(let i = 0; i<json.length; i++){
+           
+            astronauts.innerHTML += `<div class="astronaut"> <div class = 'bio'> <h3>${json[i].firstName} ${json[i].lastName}</h3> <ul> <li>Hours in space: ${json[i].hoursInSpace}</li> <li> Active: ${json[i].active}</li> <li>Skills: ${json[i].skills} </li> </ul> </div> <img class='avatar' src='${json[i].picture}'> </div>`
+            
+        }
+        
+        })
+        
+        
+    })
+    
+})
+
+//console.log(list)
